57d86a55c490e81e081b335bf7a0d4c814b4e9b3
x2nur <heisil2n@gmail.com>
2026-04-03 15:25:59 +0500

    Add storage for nvim plugins

--- start_glue.sh
+++ start_glue.sh
@@ -7,5 +7,6 @@ docker run -d --rm --name glue \
 	-p 127.0.0.1:8888:8888 -p 127.0.0.1:4040:4040 \
 	-v ~/.aws:/home/hadoop/.aws \
 	-v $(pwd):/home/hadoop/workspace \
+	-v nvimfiles:/home/hadoop/.local/share/nvim \
 	-e AWS_PROFILE=$PROFILE \
 	glue5 
