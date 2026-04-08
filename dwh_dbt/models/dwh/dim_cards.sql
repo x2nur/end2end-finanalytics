5e90369a602f11bbf890d2aff4e671eb67e95e08
x2nur <heisil2n@gmail.com>
2026-04-08 18:51:00 +0500

    Fix cards, users column names

--- dwh_dbt/models/dwh/dim_cards.sql
+++ dwh_dbt/models/dwh/dim_cards.sql
@@ -1,16 +1,16 @@
 
 select 
-	id integer
-	,client_id int 
-	,card_brand varchar 
-	,card_type varchar 
-	,card_number varchar 
-	,cvv integer 
-	,has_chip varchar 
-	,num_cards_issued integer 
-	,credit_limit decimal(102) 
-	,year_pin_last_changed integer 
-	,card_on_dark_web varchar 
-	,expire_year integer 
-	,expire_month integer 
+	id
+	,client_id
+	,card_brand
+	,card_type
+	,card_number
+	,cvv
+	,has_chip
+	,num_cards_issued
+	,credit_limit
+	,year_pin_last_changed
+	,card_on_dark_web
+	,expire_year
+	,expire_month
 from {{ source('staging', 'cards') }}
--- dwh_dbt/models/dwh/dim_users.sql
+++ dwh_dbt/models/dwh/dim_users.sql
@@ -1,18 +1,18 @@
 
-select 
-	id integer
-	, current_age integer
-	, retirement_age integer
-	, birth_year integer
-	, birth_month integer
-	, gender varchar 
-	, address varchar 
-	, latitude real
-	, longitude real
-	, per_capita_income decimal(10,2)
-	, yearly_income decimal(10,2)
-	, total_debt decimal(10,2)
-	, credit_score integer
-	, num_credit_cards integer
-	, is_retired boolean
+select
+	id
+	, current_age
+	, retirement_age
+	, birth_year
+	, birth_month
+	, gender
+	, address
+	, latitude
+	, longitude
+	, per_capita_income
+	, yearly_income
+	, total_debt
+	, credit_score
+	, num_credit_cards
+	, is_retired
 from {{ source('staging', 'users') }}
