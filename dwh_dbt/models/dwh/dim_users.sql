724cfb0ac93a2e5b5a86068cec629d04c47e933f
x2nur <heisil2n@gmail.com>
2026-04-08 16:39:17 +0500

    Users dimension

--- dwh_dbt/models/dwh/dim_users.sql
+++ dwh_dbt/models/dwh/dim_users.sql
@@ -1,4 +1,18 @@
 
 select 
-	*
+	id integer
+	, current_age integer
+	, retirement_age integer
+	, birth_year integer
+	, birth_month integer
+	, gender varchar 
+	, address varchar 
+	, latitude real
+	, longitude real
+	, per_capita_income decimal(10,2)
+	, yearly_income decimal(10,2)
+	, total_debt decimal(10,2)
+	, credit_score integer
+	, num_credit_cards integer
+	, is_retired boolean
 from {{ source('staging', 'users') }}
