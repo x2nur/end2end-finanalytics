39db24393a33f061f92c2abaaa1417ec52eab765
x2nur <heisil2n@gmail.com>
2026-04-08 19:06:56 +0500

    Extract merchants to dimension

---
+++ dwh_dbt/models/dwh/dim_merchants.sql
@@ -0,0 +1,26 @@
+
+with 
+merchant as (
+	select 
+		merchant_id -- PK
+		, merchant_city
+		, merchant_state
+		, zip
+		, mcc as merchant_code
+		, row_number() over (partition by merchant_id order by event_date) as row_num
+	from {{ source('staging', 'transactions') }}
+)
+select 
+	merchant_id
+	, merchant_city
+	, merchant_state
+	, zip
+	, mcc as merchant_code
+from merchant
+where row_num = 1
+
+
+
+-- dimenstions:
+-- - transaction type 
+-- - error type 
