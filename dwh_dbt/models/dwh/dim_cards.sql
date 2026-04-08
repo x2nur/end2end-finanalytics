4f7d5d65967caa0c550a1c5d47368dc786195ab3
x2nur <heisil2n@gmail.com>
2026-04-08 16:51:27 +0500

    Select only required columns of cards table

--- dwh_dbt/models/dwh/dim_cards.sql
+++ dwh_dbt/models/dwh/dim_cards.sql
@@ -1,4 +1,16 @@
 
 select 
-	{{ dbt_utils.star(from=source('staging', 'cards'), except=['acct_open_year', 'acct_open_month']) }}
+	id integer
+	,client_id int 
+	,card_brand varchar 
+	,card_type varchar 
+	,card_number varchar 
+	,cvv integer 
+	,has_chip varchar 
+	,num_cards_issued integer 
+	,credit_limit decimal(102) 
+	,year_pin_last_changed integer 
+	,card_on_dark_web varchar 
+	,expire_year integer 
+	,expire_month integer 
 from {{ source('staging', 'cards') }}
