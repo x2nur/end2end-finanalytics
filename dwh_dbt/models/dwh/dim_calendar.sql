f0034415eaab726a29d62385e9ad22105865c843
x2nur <heisil2n@gmail.com>
2026-04-08 16:33:03 +0500

    Calendar dimension

--- dwh_dbt/models/dwh/dim_calendar.sql
+++ dwh_dbt/models/dwh/dim_calendar.sql
@@ -10,5 +10,15 @@ date_range as (
 	}}
 )
 select 
-	*	
+	date_day::date as _date
+	, extract(year from date_day) as year
+	, extract(quarter from date_day) as quarter
+	, extract(month from date_day) as month
+	, to_char(date_day, 'Month') as full_month_name 
+	, to_char(date_day, 'Mon') as short_month_name 
+	, extract(week from date_day) as week
+	, (extract(dow from date_day) + 6) % 7 as day_of_week
+	, extract(day from date_day) as full_weekday_name
+	, to_char(date_day, 'Day') as full_day_name
+	, to_char(date_day, 'Dy') as short_day_name
 from date_range
