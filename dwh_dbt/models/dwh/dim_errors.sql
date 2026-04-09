2541fb355921edef5bc0c1c823454a55c40e8ed0
x2nur <heisil2n@gmail.com>
2026-04-09 11:38:19 +0500

    Split errors

--- dwh_dbt/models/dwh/dim_errors.sql
+++ dwh_dbt/models/dwh/dim_errors.sql
@@ -1,12 +1,17 @@
 -- Transaction error dimension
 
-with errors as (
-	select 
-		error_type
+with 
+errors0 as (
+	select split_to_array(errors, ',') as errors
 	from {{ source('staging', 'transactions') }}
+	group by 1
+), 
+errors1 as (
+	select trim(cast(error as varchar) ) as error
+	from errors0 as tab, tab.errors as error
 	group by 1
 )
 select 
-	{{ dbt_utils.generate_surrogate_key(['error_type']) }} as id 
-	, error_type
-from errors
+	{{ dbt_utils.generate_surrogate_key(['error']) }} as id 
+	, error 
+from errors1 
