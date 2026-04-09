8c372433978657cf89bdbba4ec8c5d1e4b11a98c
x2nur <heisil2n@gmail.com>
2026-04-09 21:16:45 +0500

    Make transaction type dim table incremental

--- dwh_dbt/models/dwh/dim_transaction_type.sql
+++ dwh_dbt/models/dwh/dim_transaction_type.sql
@@ -1,8 +1,19 @@
 -- Transaction type dimension
 
+{{
+	config(
+		materialized='incremental',
+		incremental_strategy='merge',
+		unique_key='transaction_type_id'
+	)
+}}
+
 with tx_types as (
 	select use_chip as transaction_type
 	from {{ source('staging', 'transactions') }}
+	{% if is_incremental() -%}
+	where event_date >= timestamp '{{ var("interval_start") }}' -- and interval_end
+	{%- endif %}
 	group by 1
 )
 select 
