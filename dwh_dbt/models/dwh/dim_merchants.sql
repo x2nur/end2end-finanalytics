3c9ce2bf084c101bd09ffff707dc22268c71d761
x2nur <heisil2n@gmail.com>
2026-04-09 21:08:36 +0500

    Make merchant dim table incremental

--- dwh_dbt/models/dwh/dim_merchants.sql
+++ dwh_dbt/models/dwh/dim_merchants.sql
@@ -1,4 +1,12 @@
 
+{{
+	config(
+		materialized='incremental',
+		incremental_strategy='merge',
+		unique_key='merchant_id'
+	)
+}}
+
 with 
 merchant as (
 	select 
@@ -7,15 +15,23 @@ merchant as (
 		, merchant_state
 		, zip
 		, mcc 
-		, row_number() over (partition by merchant_id order by event_date) as row_num
+		, row_number() over (partition by merchant_id order by event_date desc) as row_num
 	from {{ source('staging', 'transactions') }}
+	{% if is_incremental() -%}
+	where event_date >= timestamp '{{ var("interval_start") }}' -- and interval_end
+	{%- endif %}
+)
+, merchant_codes as (
+	select * 
+	from {{ source('staging', 'mcc_codes') }}
 )
 select 
 	merchant_id
 	, merchant_city
 	, merchant_state
 	, zip
-	, mcc as merchant_code
-from merchant
+	, merchant.mcc as merchant_code
+	, merchant_codes.description as merchant_code_description
+from merchant left join merchant_codes using (mcc)
 where row_num = 1
 
