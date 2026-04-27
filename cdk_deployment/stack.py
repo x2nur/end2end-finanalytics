7cec1ad7358ce4d5f406b126a0bd53e0e2aef509
x2nur <heisil2n@gmail.com>
2026-04-27 21:25:22 +0500

    Add a lambda function to resolve missing zip codes

--- cdk_deployment/stack.py
+++ cdk_deployment/stack.py
@@ -1,13 +1,33 @@
-from aws_cdk import ( 
-    Stack 
-    , aws_lambda as lmb
+import aws_cdk as cdk
+from aws_cdk import (
+    Stack,
+    aws_lambda,
+    aws_ecr,
 )
 from constructs import Construct
 
-class FinAnalyticsStack(Stack):
+class FinanalyticsStack(Stack):
 
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
 
+        # ===== Missing zip codes lamda =====
+        
+        missing_zipcodes_resolver_repo = aws_ecr.Repository.from_repository_name(
+            self, 
+            "MissingZipcodesResolverECRRepo", 
+            "missing-zipcodes-resolver"
+        )
 
+        missing_zipcodes_resolver_image = aws_lambda.DockerImageCode.from_ecr(
+            repository=missing_zipcodes_resolver_repo,
+            tag="0.7",
+        )
 
+        missing_zipcodes_resolver_lambda = aws_lambda.DockerImageFunction(
+            self,
+            "MissingZipcodesResolverLambda",
+            code=missing_zipcodes_resolver_image,
+            memory_size=512,
+            timeout=cdk.Duration.seconds(180),
+        )
